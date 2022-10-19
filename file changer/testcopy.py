7787f373b5400689665006bbc30a76f46662fef9
ShadeRemix <94280261+ShadeRemix@users.noreply.github.com>
2022-10-19 15:06:53 +0800

    Update testcopy.py

--- file changer/testcopy.py
+++ file changer/testcopy.py
@@ -3,21 +3,26 @@ import os
 import time
 import subprocess
 
-
-
-#os.popen('cp test.txt test.txt1;rm test.txt;mv test.txt1 test.txt')
-
-subprocess.call("cp test.txt test.txt1;rm test.txt;mv test.txt1 test.txt", shell=True)
-
-a = 'test.txt'
-a_file = filedate.File(a)
-
-a_file.set(
-	#created = '1999.01.01 13:00:00',
-	modified = '1999.01.01 13:00:00',
-	accessed = '1999.01.01 13:00:00'
-	#birth = '1999.01.01 13:00:00'
-)
-
-after = filedate.File(a)
-print(after.get())
+# changes created and birth time
+# gets current directory
+cwd = os.getcwd()
+# gets list of files in current directory
+files = os.listdir(cwd)
+print(cwd)
+print(files)
+# Changes time of all files in list
+for file in files:
+    filename = file
+    # calling cp to copy file to file1
+    # then shred file
+    # then mv file1 to file
+    subprocess.call("cp " + filename + " " + filename + "1;shred " + filename + ";mv " + filename + "1 " + filename,
+                    shell=True)
+    a_file = filedate.File(filename)
+    # changes modify and access
+    a_file.set(
+        modified='1999.01.01 13:00:00',
+        accessed='1999.01.01 13:00:00'
+    )
+    after = filedate.File(filename)
+    print(after.get())
